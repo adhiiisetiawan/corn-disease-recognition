69d9ce527a4e9c6f5fcc2d7568e5fdd52f4012af
adhiiisetiawan <adhisetiawan518@gmail.com>
2023-01-02 16:32:58 +0700

    change dataloader from img dataset directory

--- dataloader/corn_dataloader.py
+++ dataloader/corn_dataloader.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from base.base_dataloader import BaseDataLoader
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
 class CornDataLoader(BaseDataLoader):
@@ -9,41 +10,29 @@ class CornDataLoader(BaseDataLoader):
         self.batch_size = batch_size
         self.train_dir = train_dir
         self.val_dir = val_dir
-        
-        self.train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-            rescale=1/255,
-            validation_split=self.config.trainer.validation_split,
-            rotation_range=30,
-            width_shift_range=0.2,
-            height_shift_range=0.2,
-            shear_range=0.2,
-            zoom_range=0.2,
-            vertical_flip=True,
-            horizontal_flip=True,
-            fill_mode='nearest'
-        )
-
-        self.valid_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-            rescale=1/255,
-            validation_split=self.config.trainer.validation_split    
-        )
 
     def get_trainloader(self):
-        self.train_generator = self.train_datagen.flow_from_directory(
+        self.train_generator = image_dataset_from_directory(
             self.train_dir,
-            subset='training',
-            target_size=(self.image_size, self.image_size),
+            label_mode='categorical',
+            image_size=(self.image_size, self.image_size),
             batch_size=self.batch_size,
-            class_mode='categorical'
+            validation_split=self.config.trainer.validation_split,
+            seed=1337,
+            subset='training',
+            shuffle=True
         )
         return self.train_generator
 
     def get_validationloader(self):
-        self.valid_generator = self.valid_datagen.flow_from_directory(
+        self.valid_generator = image_dataset_from_directory(
             self.val_dir,
-            subset='validation',
-            target_size=(self.image_size, self.image_size),
+            label_mode='categorical',
+            image_size=(self.image_size, self.image_size),
             batch_size=self.batch_size,
-            class_mode='categorical'
+            validation_split=self.config.trainer.validation_split,
+            seed=1337,
+            subset='validation',
+            shuffle=True
         )
         return self.valid_generator
